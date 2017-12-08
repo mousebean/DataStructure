96f35b42350049c3a33d5ffc1cc884a0827c780c
mousebean <mousebean.163.com>
2017-12-09 00:26:27 +0800

    first commit

---
+++ DataStructure.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : DataStructure.cpp
+// Author      : MouseBean
+// Version     :
+// Copyright   : MouseBean
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "DataStructure!" << endl; // prints DataStructure!
+	return 0;
+}
